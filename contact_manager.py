f9571dabe67cb2786220d5f9ba80d1208f34a687
Tadeáš <vlcek.tadeas24@gmail.com>
2025-06-05 20:48:23 +0200

    contacts manager

---
+++ contact_manager.py
@@ -0,0 +1,33 @@
+contacts = [ 
+    {
+        "name": "Tadeáš",
+        "email": "tadeas@email.cz",
+        "telefon": "777 777 777",
+    },
+    {
+        "name": "Karel",
+        "email": "karel@email.com",
+        "telefon": "888 888 888"
+
+    }
+]
+    # Hlavní menu vypíše:
+    # 1. Zobrazit kontakty
+    # 2. Přidat kontakt
+    # 3. Ukončit
+
+    # Načte vstup od uživatele (input)
+
+    # Vytiskne
+
+def hlavni_menu():
+   print("Hlavní menu:")
+   print("1 - Zobrazit kontakty")
+   print("2. - Přidat kontakt")
+   print("3. - Ukončit Program")
+   volba = input("Zvolte možnost 1-4: ")
+   print(volba)
+   
+hlavni_menu()
+
+
